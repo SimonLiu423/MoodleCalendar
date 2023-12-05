693e24bc7d10ecdbe21ed60bb7ad6d5a5c813dc0
SimonLiu <simonliu423@gmail.com>
2023-12-05 10:25:16 +0800

    Fix(flask-app): cookie config not working

--- src/server/server_main.py
+++ src/server/server_main.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.secret_key = os.urandom(24)
 app.permanent_session_lifetime = timedelta(hours=2)
 app.config.SESSION_COOKIE_SECURE = True
-app.config.SESSION_COOKIE_SAMESITE = 'None'
-app.config.SESSION_COOKIE_SECURE = True
+app.config['SESSION_COOKIE_SAMESITE'] = 'None'
+app.config['SESSION_COOKIE_SECURE'] = True
 
 # Enable CORS for all routes
 CORS(app, supports_credentials=True,
